7babd43d0c41f2a4c85381357e028d7717cf9229
David Hirvonen <dhirvonen@elucideye.com>
2017-09-24 19:24:55 -0400

    add gtest/ctest and ci configs (.travis.yml and appveyor.yml)
    * add shared drishti-upload  hunter config
    * add automated ctest/gtest w/ optional gpu (ogles_gpgpu) support
    * add CI configurations: .travis.yml and appveyor.yml

---
+++ bin/build-appveyor.cmd
@@ -0,0 +1,21 @@
+:: Name: build-appveyor.cmd
+:: Purpose: Support readable multi-line polly.py build commands
+:: Copyright 2016-2017 Elucideye, Inc.
+::
+:: Multi-line commands are not currently supported directly in appveyor.yml files
+::
+:: See: http://stackoverflow.com/a/37647169
+
+echo POLLY_ROOT %POLLY_ROOT%
+
+python %POLLY_ROOT%\bin\polly.py ^
+--verbose ^
+--archive drishti ^
+--config "%1%" ^
+--toolchain "%2%" ^
+--test ^
+--fwd HUNTER_USE_CACHE_SERVERS=ONLY ^
+HUNTER_DISABLE_BUILDS=YES ^
+ACF_COPY_3RDPARTY_LICENSES=ON ^
+ACF_BUILD_TESTS=ON ^
+ACF_BUILD_EXAMPLES=ON
