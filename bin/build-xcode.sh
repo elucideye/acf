0b95cb712648ea92a0958dfb82927d503d549429
David Hirvonen <dhirvonen@elucideye.com>
2017-10-13 10:50:08 -0400

    add scope time logger w/ ACF_LOG_GPU_TIME option (hack benchmark in unit test)

--- bin/build-xcode.sh
+++ bin/build-xcode.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
 TOOLCHAIN=xcode
-CONFIG=MinSizeRel
+CONFIG=Release
 
 ARGS=(
     --verbose
     --toolchain ${TOOLCHAIN}
     --config ${CONFIG}
     --fwd HUNTER_CONFIGURATION_TYPES=${CONFIG}
+    ACF_USE_DRISHTI_CACHE=OFF
+    ACF_BUILD_TESTS=ON
     --jobs 8
 )
 
+#build.py ${ARGS[@]} --install --reconfig ${*}
 build.py ${ARGS[@]} --install ${*}
