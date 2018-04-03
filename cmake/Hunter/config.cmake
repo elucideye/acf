97e73f18ace32c8be831abb56397b2ae28d480d8
David Hirvonen <dhirvonen@elucideye.com>
2018-04-03 15:21:29 -0400

    simplify cmake ios logic

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,5 +1,6 @@
-string(COMPARE EQUAL "${CMAKE_OSX_SYSROOT}" "iphoneos" _is_ios)
-if(_is_ios)
+if(IOS)
+  # local workaround for protobuf compiler crash with Xcode 8.1
+  # see https://github.com/elucideye/acf/issues/41
   set(opencv_cmake_args
     WITH_PROTOBUF=OFF
     BUILD_PROTOBUF=OFF
