411b9fa3d6b1fedaa048ba9b90c13e8fcc8df8f8
David Hirvonen <dhirvonen@elucideye.com>
2019-04-28 19:02:48 -0400

    Add transfer.{h,cpp} to source if ogles_gpgpu is enabled

--- src/lib/acf/acf/sugar.cmake
+++ src/lib/acf/acf/sugar.cmake
@@ -69,8 +69,8 @@ sugar_files(ACF_TEST_SRCS
 if(ACF_BUILD_OGLES_GPGPU)
   # Public GPUACF classes added to the main library
   sugar_files(ACF_HDRS_PUBLIC GPUACF.h)
-  sugar_files(ACF_HDRS convert.h) 
-  sugar_files(ACF_SRCS GPUACF.cpp convert.cpp)
+  sugar_files(ACF_HDRS convert.h transfer.h)
+  sugar_files(ACF_SRCS GPUACF.cpp convert.cpp transfer.cpp)
 
   # All other shaders to in the acf_shaders utility lib for reuse
   sugar_files(ACF_GPU_HDRS
